68743d807483341683385d6217b06e2e6c58a7a6
Tse <33410742+ThorShockU@users.noreply.github.com>
2018-06-05 23:35:22 +0800

    Update Apprenticeship_irl.py

--- Apprenticeship_irl.py
+++ Apprenticeship_irl.py
@@ -6,8 +6,54 @@ juce.sysu@gmail.com
 """
 
 import numpy as np
-from . import value_iteration
 import time 
+
+def optimal_value(n_states, n_actions, transition_probabilities, reward,
+                  discount, threshold=1e-2):
+    
+    v = np.zeros(n_states)
+
+    diff = float("inf")
+    while diff > threshold:
+        diff = 0
+        for s in range(n_states):
+            max_v = float("-inf")
+            for a in range(n_actions):
+                tp = transition_probabilities[s, a, :]
+                max_v = max(max_v, np.dot(tp, reward + discount*v))
+
+            new_diff = abs(v[s] - max_v)
+            if new_diff > diff:
+                diff = new_diff
+            v[s] = max_v
+
+    return v
+
+def find_policy(n_states, n_actions, transition_probabilities, reward, discount,
+                threshold=1e-2, v=None, stochastic=True):
+ 
+    if v is None:
+        v = optimal_value(n_states, n_actions, transition_probabilities, reward,
+                          discount, threshold)
+
+    if stochastic:
+        # Get Q using equation 9.2 from Ziebart's thesis.
+        Q = np.zeros((n_states, n_actions))
+        for i in range(n_states):
+            for j in range(n_actions):
+                p = transition_probabilities[i, j, :]
+                Q[i, j] = p.dot(reward + discount*v)
+        Q -= Q.max(axis=1).reshape((n_states, 1))  # For numerical stability.
+        Q = np.exp(Q)/np.exp(Q).sum(axis=1).reshape((n_states, 1))
+        return Q
+
+    def _policy(s):
+        return max(range(n_actions),
+                   key=lambda a: sum(transition_probabilities[s, a, k] *
+                                     (reward[k] + discount * v[k])
+                                     for k in range(n_states)))
+    policy = np.array([_policy(s) for s in range(n_states)])
+    return policy
 
 def compute_feature_expectations(feature_matrix, trajectories):
     '''
